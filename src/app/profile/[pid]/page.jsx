19782031e03a6cdbfe3082c546d5038df9f3a441
EasinTanvir <easintanvir102@gmail.com>
2023-08-17 12:17:36 +0600

    changes

--- src/app/profile/[pid]/page.jsx
+++ src/app/profile/[pid]/page.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
-const Profiledetaiuls = ({ params }: any) => {
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const Profiledetaiuls = ({ params }) => {
   const [user, setUser] = useState("");
   const userId = params.pid;
 
